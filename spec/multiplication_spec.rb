690fc5dc0b25e2ce2d338c6b9fc0435dcc59aec7
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2022-05-23 22:12:34 +0700

    Why: Implemented triangulation technique

--- spec/multiplication_spec.rb
+++ spec/multiplication_spec.rb
@@ -10,17 +10,28 @@ class Dollar
   def times(another_number)
     Dollar.new(amount * another_number)
   end
+
+  def equals(another_dollar)
+    another_dollar.amount == amount
+  end
 end
 
 RSpec.describe 'Multiplication' do
-  describe 'multiple' do
-    it 'multiplies two numbers' do
+  describe '#times' do
+    it 'can multiply continuously with the same amount' do
       five = Dollar.new(5)
       product = five.times(2)
       expect(product.amount).to eq(10)
 
       product = five.times(3)
       expect(product.amount).to eq(15)
+    end
+  end
+
+  describe '#equals' do
+    it 'can test equality' do
+      expect(Dollar.new(5).equals(Dollar.new(5))).to be_truthy
+      expect(Dollar.new(5).equals(Dollar.new(6))).to be_falsy
     end
   end
 end
