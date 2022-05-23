341aeadf26086f55626bbbb32f14c5fb1d51369e
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2022-05-23 22:22:01 +0700

    Why: Make amount private and add pry to spec

--- spec/multiplication_spec.rb
+++ spec/multiplication_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class Dollar
-  attr_reader :amount
+require 'pry-byebug'
 
+class Dollar
   def initialize(amount)
     @amount = amount
   end
@@ -14,17 +14,19 @@ class Dollar
   def equals(another_dollar)
     another_dollar.amount == amount
   end
+
+  private
+
+  attr_reader :amount
 end
 
 RSpec.describe 'Multiplication' do
   describe '#times' do
     it 'can multiply continuously with the same amount' do
       five = Dollar.new(5)
-      product = five.times(2)
-      expect(product.amount).to eq(10)
+      expect(Dollar.new(10)).to eq(five.times(2))
 
-      product = five.times(3)
-      expect(product.amount).to eq(15)
+      expect(Dollar.new(15)).to eq(five.times(3))
     end
   end
 
