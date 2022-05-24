673545a616219a79bbe522b78b13f8b78fa4c378
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2022-05-24 22:00:59 +0700

    Why: Refactor and make amount private

--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -11,9 +11,11 @@ class Dollar
     Dollar.new(amount * another_number)
   end
 
-  def equals(another_dollar)
-    another_dollar.amount == amount
+  def ==(other)
+    amount == other.send(:amount)
   end
+
+  alias equals ==
 
   private
 
@@ -21,19 +23,19 @@ class Dollar
 end
 
 RSpec.describe 'Multiplication' do
+  describe '#equals' do
+    it 'can test equality' do
+      expect(Dollar.new(5).equals(Dollar.new(5))).to be_truthy
+      expect(Dollar.new(5).equals(Dollar.new(6))).to be_falsy
+    end
+  end
+
   describe '#times' do
     it 'can multiply continuously with the same amount' do
       five = Dollar.new(5)
       expect(Dollar.new(10)).to eq(five.times(2))
 
       expect(Dollar.new(15)).to eq(five.times(3))
-    end
-  end
-
-  describe '#equals' do
-    it 'can test equality' do
-      expect(Dollar.new(5).equals(Dollar.new(5))).to be_truthy
-      expect(Dollar.new(5).equals(Dollar.new(6))).to be_falsy
     end
   end
 end
